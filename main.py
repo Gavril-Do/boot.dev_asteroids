a0183880bd962d7f31495fc089f724fdf4e50c64
Gavril-Do <17620963+Gavril-Do@users.noreply.github.com>
2024-09-24 10:17:26 -0700

    Ch 2: Player, 2: Game Loop, three: added display

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ from constants import *
 def main():
 	pygame.init
 	print("Starting asteroids!")
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	while True:
+		screen.fill('black')
+		pygame.display.flip()
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
 
 
 if __name__ == "__main__":
