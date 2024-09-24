5eb9cdd6af9c7b315f1e3da88e796f921d59c270
Gavril-Do <17620963+Gavril-Do@users.noreply.github.com>
2024-09-24 09:29:51 -0700

    Ch 2: Player, 2: Game Loop, One

--- main.py
+++ main.py
@@ -2,12 +2,22 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from pygame.locals import *
 from constants import *
 
 def main():
+	pygame.init
 	print("Starting asteroids!")
 	print(f'Screen width: {SCREEN_WIDTH}')
 	print(f'Screen height: {SCREEN_HEIGHT}')
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	while True:
+		pygame.surface.fill(screen, 'black')
+		pygame.surface.flip()
+		for event in pygame.event.get():
+			if event.type -- pygame.QUIT:
+				return
+
 
 if __name__ == "__main__":
 	main()
