8d0d01a6ec4ac9a6f144991105c0b30e4cd637ce
Gavril-Do <17620963+Gavril-Do@users.noreply.github.com>
2024-09-23 20:38:19 -0700

    ch 2: Player, 1: modules

--- main.py
+++ main.py
@@ -2,9 +2,12 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
 	print("Starting asteroids!")
+	print(f'{SCREEN_WIDTH}')
+	print(f'{SCREEN_HEIGHT}')
 
 if __name__ == "__main__":
 	main()
