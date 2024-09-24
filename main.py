0892daa13727943a2778001f065d4c1fa43b2e6d
Gavril-Do <17620963+Gavril-Do@users.noreply.github.com>
2024-09-24 10:32:04 -0700

    Ch2: Player, 3:FPS

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from constants import *
 def main():
 	pygame.init
 	print("Starting asteroids!")
+	clock = pygame.time.Clock()
+	d_time = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	while True:
 		screen.fill('black')
@@ -15,6 +17,7 @@ def main():
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
+		d_time = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
