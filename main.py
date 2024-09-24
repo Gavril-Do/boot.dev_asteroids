c01075b011a88bceb78e4c0e30bf615b34aa3b6d
Gavril-Do <17620963+Gavril-Do@users.noreply.github.com>
2024-09-24 13:34:36 -0700

    Ch2: Player, 5: Draw Player, Final

--- main.py
+++ main.py
@@ -2,24 +2,26 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-from pygame.locals import *
 from constants import *
 from player import *
 
 def main():
 	pygame.init
-	print("Starting asteroids!")
-	clock = pygame.time.Clock()
-	d_time = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	player1 = player((SCREEN_WIDTH /2), (SCREEN_HEIGHT / 2), PLAYER_RADIUS)
+	clock = pygame.time.Clock()
+	player1 = player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+	d_time = 0
+
 	while True:
-		screen.fill('black')
-		pygame.display.flip()
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
+		
+		screen.fill('black')
 		player1.draw(screen)
+		pygame.display.flip()
+
+		# limit to 60 FPS
 		d_time = clock.tick(60) / 1000
 
 
