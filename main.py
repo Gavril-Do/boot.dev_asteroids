1fbe1b9c5b1d31713bfb5b43afbe56d923c55f58
Gavril-Do <17620963+Gavril-Do@users.noreply.github.com>
2024-09-24 10:16:36 -0700

    Ch 2: Player, 2: Game Loop, two: removed print screen_width and screen_height

--- main.py
+++ main.py
@@ -8,15 +8,6 @@ from constants import *
 def main():
 	pygame.init
 	print("Starting asteroids!")
-	print(f'Screen width: {SCREEN_WIDTH}')
-	print(f'Screen height: {SCREEN_HEIGHT}')
-	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	while True:
-		pygame.surface.fill(screen, 'black')
-		pygame.surface.flip()
-		for event in pygame.event.get():
-			if event.type -- pygame.QUIT:
-				return
 
 
 if __name__ == "__main__":
