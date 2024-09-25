eb84a4ebbc3ff2724a6463e468fe7567a04f710c
Gavril-Do <17620963+Gavril-Do@users.noreply.github.com>
2024-09-24 22:50:13 -0700

    Ch2: Player, 8: Groups

--- main.py
+++ main.py
@@ -3,13 +3,20 @@
 # throughout this file
 import pygame
 from constants import *
-from player import *
+from player import Player
 
 def main():
 	pygame.init
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	clock = pygame.time.Clock()
-	player1 = player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+
+	Player.containers = (updatable, drawable)
+
+	player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+
 	d_time = 0
 
 	while True:
@@ -17,9 +24,15 @@ def main():
 			if event.type == pygame.QUIT:
 				return
 		
+		for obj in updatable:
+			obj.update(d_time)
+
 		screen.fill('black')
-		player1.draw(screen)
-		player1.update(d_time)
+
+		for obj in drawable:
+			obj.draw(screen)
+	#	player1.draw(screen)
+	#	player1.update(d_time)
 		pygame.display.flip()
 
 		# limit to 60 FPS
